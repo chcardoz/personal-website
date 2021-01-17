53b9d526eb9f768b9c04275caaf4b0064138656b
Chris-Cardoza <chcardoz@iu.edu>
2021-01-17 20:37:32 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: "Chris Cardoza",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
